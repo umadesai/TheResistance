b12197613ca04619294f70eadea40460d553301e
Serena Chen <serenazychen@gmail.com>
2016-12-01 23:29:22 -0500

    all the backwards movement

--- src/arduino/ResistanceTester/ResistanceTester.ino
+++ src/arduino/ResistanceTester/ResistanceTester.ino
@@ -4,7 +4,7 @@
 */
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
-#include <Stepper.h>
+//#include <Stepper.h>
 
 int analogPin = 0;
 int raw = 0;
@@ -15,29 +15,30 @@ float R2 = 0;
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
-Stepper motor(64, 2, 3, 4, 5);
+int IN1 = 2, IN2 = 3, IN3 = 4, IN4 = 5;
 
 boolean testResistance = true;
-int numFastSteps = 342; //total of 2055, divided by 2, roounded down
+int numFastSteps = 342; //total of 2055, divided by 2, rounded down
 int numSlowSteps = 343; //total of 2055, divided by 2, rounded up
+
+int currentStep = 0;
 
 void setup()
 {
   Serial.begin(9600);
 
   //lcd
-  //  lcd.begin(20, 4);
-  //  lcd.backlight();
-  //  lcd.setCursor(0, 0); //Start at character 0 on line 0
-  //  lcd.print("The Resistance");
-  //  delay(1000);
+    lcd.begin(20, 4);
+    lcd.backlight();
+    lcd.setCursor(0, 0); //Start at character 0 on line 0
+    lcd.print("The Resistance");
+    delay(1000);
 
   //stepper
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  motor.setSpeed(200);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
 void loop()
@@ -46,21 +47,18 @@ void loop()
     int stepSize = 1;
     for (int i = 0; i <= numSlowSteps; i += stepSize) {
       //since the stepper is blocking, we want to step a step at a time.
-      motor.step(stepSize);
+      stepBackwards(stepSize);
       delay(10);
       readResistance();
-      delay(10);
     }
   }
   else {
     Serial.println("FAST\n\n\n\n");
-    motor.step(numFastSteps);
+    stepBackwards(numFastSteps);
   }
   delay(100);
   testResistance = !testResistance;
 }
-
-
 
 void readResistance() {
   //replace below code with code to test resistance
@@ -78,10 +76,65 @@ void readResistance() {
     Serial.print("R2: ");
     Serial.println(R2);
 
-    //    lcd.setCursor(0, 0); //Start at character 0 on line 0
-    //    lcd.print("The resistance is:");
-    //    lcd.setCursor(0, 1);
-    //    lcd.print(String(R2));
+        lcd.setCursor(0, 0); //Start at character 0 on line 0
+        lcd.print("The resistance is:");
+        lcd.setCursor(0, 1);
+        lcd.print(String(R2));
   }
+}
+
+void stepBackwards(int numSteps) {
+  long lastTime = micros();
+  int i = 0;
+  while(i<=numSteps) {
+   if((micros() - lastTime) >= 50) {
+      singleStep();
+      lastTime = micros();
+      i++;
+   }
+    
+  }
+}
+
+void singleStep(){
+   switch (currentStep) {
+      case 0:
+        writeStepperPins(LOW, LOW, LOW, HIGH);
+        break;
+      case 1:
+        writeStepperPins(LOW, LOW, HIGH, HIGH);
+        break;
+      case 2:
+        writeStepperPins(LOW, LOW, HIGH, LOW);
+        break;
+      case 3:
+        writeStepperPins(LOW, HIGH, HIGH, LOW);
+        break;
+      case 4:
+        writeStepperPins(LOW, HIGH, LOW, LOW);
+        break;
+      case 5:
+        writeStepperPins(HIGH, HIGH, LOW, LOW);
+        break;
+      case 6:
+        writeStepperPins(HIGH, LOW, LOW, LOW);
+        break;
+      case 7:
+        writeStepperPins(HIGH, LOW, LOW, HIGH);
+        break;
+      default:
+        writeStepperPins(LOW, LOW, LOW, LOW);
+        break;
+    }
+    currentStep-=(testResistance? 1:3);
+    if (currentStep < 0)
+      currentStep = 7;
+}
+
+void writeStepperPins(int in1Val, int in2Val, int in3Val, int in4Val) {
+  digitalWrite(IN1, in1Val);
+  digitalWrite(IN2, in2Val);
+  digitalWrite(IN3, in3Val);
+  digitalWrite(IN4, in4Val);
 }
 
--- src/arduino/StepperTest/StepperTest.ino
+++ src/arduino/StepperTest/StepperTest.ino
@@ -1,27 +1,34 @@
 #include <Stepper.h>
 
-Stepper motor(64, 2, 3, 4, 5);
+//Stepper motor(64, 2, 3, 4, 5);
+
+int IN1 = 2, IN2 = 3, IN3 = 4, IN4 = 5;
+int currentStep = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  motor.setSpeed(300);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+//  motor.setSpeed(300);
   Serial.begin(9600);
+  Serial.println("Start");
 }
 
 int numLoops = 0;
 
 void loop() {
   //using serial to control when the stepper starts and stops
+  int count = 0;
   while(Serial.read() == -1){
    //step until the user sends something over serial
-   motor.step(1);
+   stepBackwards(1);
+   count++;
    delay(10);
   }
-  
+
+  Serial.println(count);
   while(Serial.read() != -1); //clear buffer
   
   while(Serial.read() == -1){
@@ -31,3 +38,59 @@ void loop() {
 
   while(Serial.read() != -1); //clear buffer
 }
+
+void stepBackwards(int numSteps) {
+  long lastTime = micros();
+  int i = 0;
+  while(i<numSteps) {
+   if((micros() - lastTime) >= 50) {
+      singleStep();
+      lastTime = micros();
+      i++;
+   }
+    
+  }
+}
+
+void singleStep(){
+   switch (currentStep) {
+      case 0:
+        writeStepperPins(LOW, LOW, LOW, HIGH);
+        break;
+      case 1:
+        writeStepperPins(LOW, LOW, HIGH, HIGH);
+        break;
+      case 2:
+        writeStepperPins(LOW, LOW, HIGH, LOW);
+        break;
+      case 3:
+        writeStepperPins(LOW, HIGH, HIGH, LOW);
+        break;
+      case 4:
+        writeStepperPins(LOW, HIGH, LOW, LOW);
+        break;
+      case 5:
+        writeStepperPins(HIGH, HIGH, LOW, LOW);
+        break;
+      case 6:
+        writeStepperPins(HIGH, LOW, LOW, LOW);
+        break;
+      case 7:
+        writeStepperPins(HIGH, LOW, LOW, HIGH);
+        break;
+      default:
+        writeStepperPins(LOW, LOW, LOW, LOW);
+        break;
+    }
+    currentStep-=2;
+    if (currentStep < 0)
+      currentStep = 7;
+}
+
+void writeStepperPins(int in1Val, int in2Val, int in3Val, int in4Val) {
+  digitalWrite(IN1, in1Val);
+  digitalWrite(IN2, in2Val);
+  digitalWrite(IN3, in3Val);
+  digitalWrite(IN4, in4Val);
+}
+
