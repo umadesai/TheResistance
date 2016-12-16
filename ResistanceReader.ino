f9fd47741b30968b8224a69260234b68b759bf86
abuchele <anna.buchele@students.olin.edu>
2016-12-15 21:22:15 -0500

    Fixed sorting wheel

---
+++ ResistanceReader.ino
@@ -0,0 +1,225 @@
+float r1 = 10000.0;
+int vin = 1023;
+
+double unknownR = 0.0;
+float ratio = 0.0;
+
+const float desiredRatio = 0.5;
+
+const int RELAY_ON = LOW, RELAY_OFF = HIGH;
+
+const float SUCCESS_THRESHOLD = 0.05, WAY_OFF_THRESHOLD = 0.3; /*CHANGE THESE THRESHOLDS IF NECESSARY*/
+
+//guesses
+float bestRatioDiff = 1.0;
+double bestResistanceGuess = 0.0;
+byte bestVal = 0;
+
+int lowerBoundPot, upperBoundPot;
+
+
+
+bool nextRelay = false;
+int state = 0;
+
+
+// Main methods-------------------------------------------------------------------------------
+
+float bestResistance() {
+  digitalWrite(RELAY3, RELAY_ON);
+  delay(50);
+
+  setStartingValues();
+
+  //testing all 3 resistors/digital potentiometers
+  for (int i = 0; i < 3; i++) {
+    setRelays(i);
+    Serial.println(i + "---------------------------------------------");
+    int digiPotVal = 0; //store it as an int, so we can chack if it goes over. Cast it as a byte before we send it.
+
+    //    while (digiPotVal < 256) {
+    nextRelay = false;
+    while (!nextRelay) {
+      writeDigiPotResistance((byte)digiPotVal, i);
+      readResistance();
+
+
+      Serial.println(digiPotVal);
+      setBestRatio(digiPotVal);
+
+      if (state == 0) {
+        digiPotVal += 20;         // increment value by 20s
+        if (digiPotVal > 240) { // if it passes 240th position (max while incrementing in 20s)
+          digiPotVal = bestVal - 20; // go back to val 20 points below where lowest value was recorded
+          state = 1; // change to state 1
+        }
+      }
+
+
+      else if (state == 1) {
+        digiPotVal += 10;         // increment value by 10s
+        if (digiPotVal >= (bestVal + 20)) { // if it passes 256th position (max), reset to zero
+          state = 2;
+          digiPotVal = bestVal - 10;
+        }                 // start over from lowest value
+        if (digiPotVal >= (250)) { // if it passes 256th position (max), reset to zero
+          state = 2;
+          digiPotVal = bestVal - 10;
+        }
+      }
+
+      else if (state == 2) {
+        digiPotVal++;         // increment value by 1
+        if (digiPotVal >= (bestVal + 10) || digiPotVal > 255) { // if it passes 256th position (max), reset to zero
+          state = 3;
+        }                 // start over from lowest value
+      }
+
+      else if (state == 3) {
+        //        if (bestRatioDiff > WAY_OFF_THRESHOLD) { //if the lowest vout difference is higher than 100^2, we should try again at the next relay
+        nextRelay = true;
+        state = 0;
+      }
+
+
+      //      float ratioDiff = abs(ratio - desiredRatio);
+
+      //      //set the next value of digiPotVal based on how far we are from the ideal
+      //      if (ratioDiff > WAY_OFF_THRESHOLD) {
+      //        digiPotVal += 20;
+      //      }
+      //      else if (ratioDiff < SUCCESS_THRESHOLD) {
+      //        //we found a really great value, quit looking for better values
+      //        i = 4;
+      //        break;
+      //      }
+      //      else {
+      //        digiPotVal++;
+      //      }
+    }
+  }
+
+
+  //printing results
+  if (bestRatioDiff < 1.0)
+    printResults();
+
+  digitalWrite(RELAY3, RELAY_OFF);
+  return bestResistanceGuess;
+}
+
+void readResistance() {
+  int vout = analogRead(analogPin);
+  vin = analogRead(vinPin);
+  if (vout)
+  {
+    ratio = ((float)vout) / vin;
+    unknownR = r1 * (vin - vout) / vout; // V = IR, R2 = R1*(Vin - Vout)/Vout
+  }
+  Serial.print(r1);
+  Serial.print(" ");
+  Serial.print(vout);
+  Serial.print(" ");
+  Serial.print(vin);
+  Serial.print(" ");
+  Serial.println(unknownR);
+}
+
+// Helper methods------------------------------------------------------------------------------
+
+boolean setBestRatio(byte digiPotVal) {
+  //find the difference between the current ratio and the desired ratio (where both resistors are the same)
+  float ratioDiff = abs(ratio - desiredRatio);
+
+  //set the best result so far
+  if (ratioDiff < bestRatioDiff) {
+    bestRatioDiff = ratioDiff;
+    bestResistanceGuess = unknownR;
+    bestVal = (byte)digiPotVal;
+    return true;
+  }
+  return false;
+}
+
+/**
+   There are 3 resistors that the relays can be programmed to test with, numbered 0, 1, 2.
+   Given the number of a resistor, this function turns on/off the proper relays.
+*/
+void setRelays(int whichResistor) {
+  switch (whichResistor) {
+    case 0:
+      digitalWrite(RELAY1, RELAY_ON);
+      break;
+    case 1:
+      digitalWrite(RELAY1, RELAY_OFF);
+      digitalWrite(RELAY2, RELAY_OFF);
+      break;
+    case 2:
+      digitalWrite(RELAY1, RELAY_OFF);
+      digitalWrite(RELAY2, RELAY_ON);
+      break;
+  }
+  delay(100);
+}
+
+void setR1(int whichResistor) {
+  switch (whichResistor) {
+    case 0:
+      r1 = 97.6;
+      break;
+    case 1:
+      r1 = 10e3;
+      break;
+    case 2:
+      r1 = 931e3;
+      break;
+  }
+}
+
+void printResults() {
+  Serial.print("R2: ");
+  Serial.println(bestResistanceGuess);
+
+  if (USE_LCD) {
+    lcd.clear();
+    lcd.setCursor(0, 0); //Start at character 0 on line 0
+    lcd.print("The resistance is:");
+    lcd.setCursor(0, 1);
+    lcd.print(String(bestResistanceGuess));
+
+  }
+}
+
+void setStartingValues() {
+  bestRatioDiff = 1.0;
+  unknownR = 0.0;
+  ratio = 0.0;
+  bestVal = 0;
+}
+
+void writeDigiPotResistance(byte val, int whichResistor) {
+  int address = 0;
+  if (whichResistor == 0) {
+    //10k
+    address = 44;
+    r1 = 30.524 + ((float)val * 38.34);
+  }
+  else if (whichResistor == 1) {
+    //100K
+    address = 46;
+    r1 = 174.17 + ((float)val * 384.62);
+  }
+  else if (whichResistor == 2) {
+    //1M
+    address = 45;
+    r1 = (-0.1644 * (float)(pow(val, 3))) + (64.274 * (float)(pow(val, 2))) - (1652.7 * (float) val) + 153635;
+  }
+  Wire.beginTransmission(address);
+  Wire.write(byte(0x00));            // sends instruction byte
+  Wire.write(val);             // sends potentiometer value byte
+  Wire.endTransmission();     // stop transmittings
+}
+
+
+
+
---
+++ ResistanceTester.ino
@@ -0,0 +1,176 @@
+/*
+   The Resistance
+*/
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <Servo.h>
+//#include <Stepper.h>
+
+const int analogPin = A0, vinPin = A1;
+const int RELAY1 = 11, RELAY2 = 12, RELAY3 = 13;
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+const boolean USE_LCD = true;
+
+//stepper in
+int IN1 = 3, IN2 = 4, IN3 = 5, IN4 = 6;
+int currentStep = 0; //stepper control
+
+//button
+int BUTTON = 2;
+
+//servo
+Servo sorter;
+
+void setup()
+{
+//  Wire.begin();
+  Serial.begin(9600);
+  Serial.println("The Revolution");
+  //stepper
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  //servo
+  sorter.attach(10);
+  //relay
+  pinMode(RELAY1, OUTPUT);
+  pinMode(RELAY2, OUTPUT);
+  pinMode(RELAY3, OUTPUT);
+
+  //lcd
+  if (USE_LCD) {
+    lcd.begin(20, 4);
+    lcd.backlight();
+    delay(100);
+    lcd.clear();
+    lcd.setCursor(3, 0); //Start at character 4 on line 0
+    lcd.print("The Resistance");
+    singleStep(); //stepping the stepper motor, because that somehow affects the bg of the lcd
+    lcd.setCursor(6, 1);
+    lcd.print("presents");
+    singleStep(); //the stupidest thing ever
+    lcd.setCursor(3, 3);
+    lcd.print("The Revolution");
+    singleStep();
+    delay(3500);
+  }
+
+  //calibration
+  pinMode(BUTTON, INPUT);
+  calibrate();
+}
+
+void loop()
+{
+  float resistance = bestResistance();
+  setSorterWheelPos(resistance);
+  for (int i = 0; i < 682; i++) {
+    singleStep();
+    delay(10);
+  }
+
+}
+
+void calibrate(){
+   if (USE_LCD) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Press the button");
+    lcd.setCursor(0, 1);
+    lcd.print("");
+    lcd.print("when a notch on the");
+    lcd.setCursor(0, 2);
+    lcd.print("wheel is aligned");
+    lcd.setCursor(0, 3);
+    lcd.print("with the ramp");
+  }
+
+  boolean initButton = digitalRead(BUTTON);
+  while (digitalRead(BUTTON) == initButton) {
+    singleStep();
+    delay(20);
+  }
+
+  delay(250);
+  for (int i = 0; i < 341; i++) {
+    singleStep();
+    delay(5);
+  }
+
+  if (USE_LCD) {
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("When you are ready,");
+    lcd.setCursor(0, 1);
+    lcd.print("drop a resistor onto");
+    lcd.setCursor(6, 2);
+    lcd.print("the ramp");
+    delay(3500);
+  }
+}
+
+void setSorterWheelPos(float resistance) {
+  if (resistance < 1000) {
+    sorter.write(0);
+  }
+  else if (resistance < 5000) {
+    sorter.write(60);
+  }
+  else if (resistance < 10000) {
+    sorter.write(120);
+  }
+  else if (resistance < 50000) {
+    sorter.write(180);
+  }
+  else if (resistance < 100000) {
+    sorter.write(240);
+  }
+  else {
+    sorter.write(300);
+  }
+}
+
+void singleStep() {
+  switch (currentStep) {
+    case 0:
+      writeStepperPins(LOW, LOW, LOW, HIGH);
+      break;
+    case 1:
+      writeStepperPins(LOW, LOW, HIGH, HIGH);
+      break;
+    case 2:
+      writeStepperPins(LOW, LOW, HIGH, LOW);
+      break;
+    case 3:
+      writeStepperPins(LOW, HIGH, HIGH, LOW);
+      break;
+    case 4:
+      writeStepperPins(LOW, HIGH, LOW, LOW);
+      break;
+    case 5:
+      writeStepperPins(HIGH, HIGH, LOW, LOW);
+      break;
+    case 6:
+      writeStepperPins(HIGH, LOW, LOW, LOW);
+      break;
+    case 7:
+      writeStepperPins(HIGH, LOW, LOW, HIGH);
+      break;
+    default:
+      writeStepperPins(LOW, LOW, LOW, LOW);
+      break;
+  }
+  currentStep += 2;
+  if (currentStep >= 7)
+    currentStep = 0;
+}
+
+void writeStepperPins(int in1Val, int in2Val, int in3Val, int in4Val) {
+  digitalWrite(IN1, in1Val);
+  digitalWrite(IN2, in2Val);
+  digitalWrite(IN3, in3Val);
+  digitalWrite(IN4, in4Val);
+}
+
